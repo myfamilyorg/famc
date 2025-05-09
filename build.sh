9df8bd011c92df971bf26a15a3e74e49b2de7893
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-09 15:30:20 -0700

    reduce to core only

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 set -e
 export RUSTC_VERSION=1.29.0 MRUSTC_TARGET_VER=1.29 OUTDIR_SUF=-1.29.0
 make
-make -f minicargo.mk LIBS $@
+mkdir -p output-1.29.0
+./bin/mrustc ./rustc-1.29.0-src/src/libcore/lib.rs \
+	-o output-1.29.0/libcore.rlib \
+	-C emit-depfile=output-1.29.0/libcore.rlib.d \
+	--cfg debug_assertions \
+	-O -L output-1.29.0 \
+	--crate-name core \
+	--crate-type rlib \
+	--crate-tag 0_0_0 > output-1.29.0/libcore.rlib_dbg.txt
