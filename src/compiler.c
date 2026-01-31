e4fa5703c63dd72d82a0598d96455b81668b4b37
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-01-31 15:11:08 -0800

    additional compiler reduced complexity

--- src/compiler.c
+++ src/compiler.c
@@ -1,3 +1,23 @@
+/* Ensure compiler compat */
+#ifndef __famc__
+
+#define STATIC_ASSERT(condition, message) \
+	typedef unsigned char static_assert_##message[(condition) ? 1 : -1]
+
+STATIC_ASSERT(sizeof(char) == 1, char_size);
+STATIC_ASSERT(sizeof(unsigned char) == 1, uchar_size);
+STATIC_ASSERT(sizeof(short) == 2, short_size);
+STATIC_ASSERT(sizeof(unsigned short) == 2, ushort_size);
+STATIC_ASSERT(sizeof(int) == 4, int_size);
+STATIC_ASSERT(sizeof(unsigned int) == 4, uint_size);
+STATIC_ASSERT(sizeof(unsigned) == 4, u2int_size);
+STATIC_ASSERT(sizeof(long) == 8, long_size);
+STATIC_ASSERT(sizeof(unsigned long) == 8, ulong_size);
+STATIC_ASSERT(sizeof(void *) == 8, os_64_bit);
+STATIC_ASSERT(__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__, little_endian);
+
+#endif /* __famc__ */
+
 struct io_cqring_offsets {
 	unsigned head;
 	unsigned tail;
@@ -87,110 +107,121 @@ struct Sync {
 	unsigned *cq_mask;
 };
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-const-variable"
+/*
+int IORING_OP_NOP = 0;
+int IORING_OP_READV = 1;
+int IORING_OP_WRITEV = 2;
+int IORING_OP_FSYNC = 3;
+int IORING_OP_READ_FIXED = 4;
+int IORING_OP_WRITE_FIXED = 5;
+int IORING_OP_POLL_ADD = 6;
+int IORING_OP_POLL_REMOVE = 7;
+int IORING_OP_SYNC_FILE_RANGE = 8;
+int IORING_OP_SENDMSG = 9;
+int IORING_OP_RECVMSG = 10;
+int IORING_OP_TIMEOUT = 11;
+int IORING_OP_TIMEOUT_REMOVE = 12;
+int IORING_OP_ACCEPT = 13;
+int IORING_OP_ASYNC_CANCEL = 14;
+int IORING_OP_LINK_TIMEOUT = 15;
+int IORING_OP_CONNECT = 16;
+int IORING_OP_FALLOCATE = 17;
+int IORING_OP_OPENAT = 18;
+int IORING_OP_CLOSE = 19;
+int IORING_OP_FILES_UPDATE = 20;
+int IORING_OP_STATX = 21;
+int IORING_OP_READ = 22;
+int IORING_OP_WRITE = 23;
+int IORING_OP_FADVISE = 24;
+int IORING_OP_MADVISE = 25;
+int IORING_OP_SEND = 26;
+int IORING_OP_RECV = 27;
+int IORING_OP_OPENAT2 = 28;
+int IORING_OP_EPOLL_CTL = 29;
+int IORING_OP_SPLICE = 30;
+int IORING_OP_PROVIDE_BUFFERS = 31;
+int IORING_OP_REMOVE_BUFFERS = 32;
+int IORING_OP_TEE = 33;
+int IORING_OP_SHUTDOWN = 34;
+int IORING_OP_RENAMEAT = 35;
+int IORING_OP_UNLINKAT = 36;
+int IORING_OP_MKDIRAT = 37;
+int IORING_OP_SYMLINKAT = 38;
+int IORING_OP_LINKAT = 39;
+int IORING_OP_MSG_RING = 40;
+int IORING_OP_FSETXATTR = 41;
+int IORING_OP_SETXATTR = 42;
+int IORING_OP_FGETXATTR = 43;
+int IORING_OP_GETXATTR = 44;
+int IORING_OP_SOCKET = 45;
+int IORING_OP_URING_CMD = 46;
+int IORING_OP_SEND_ZC = 47;
+int IORING_OP_SENDMSG_ZC = 48;
+int IORING_OP_READ_MULTISHOT = 49;
+int IORING_OP_WAITID = 50;
+int IORING_OP_FUTEX_WAIT = 51;
+int IORING_OP_FUTEX_WAKE = 52;
+int IORING_OP_FUTEX_WAITV = 53;
+int IORING_OP_FIXED_FD_INSTALL = 54;
+int IORING_OP_FTRUNCATE = 55;
+int IORING_OP_BIND = 56;
+int IORING_OP_LISTEN = 57;
+int IORING_OP_RECV_ZC = 58;
+int IORING_OP_EPOLL_WAIT = 59;
+int IORING_OP_READV_FIXED = 60;
+int IORING_OP_WRITEV_FIXED = 61;
+int IORING_OP_PIPE = 62;
+int IORING_OP_NOP128 = 63;
+int IORING_OP_URING_CMD128 = 64;
+int IORING_OP_LAST = 65;
 
-static const int IORING_OP_NOP = 0;
-static const int IORING_OP_READV = 1;
-static const int IORING_OP_WRITEV = 2;
-static const int IORING_OP_FSYNC = 3;
-static const int IORING_OP_READ_FIXED = 4;
-static const int IORING_OP_WRITE_FIXED = 5;
-static const int IORING_OP_POLL_ADD = 6;
-static const int IORING_OP_POLL_REMOVE = 7;
-static const int IORING_OP_SYNC_FILE_RANGE = 8;
-static const int IORING_OP_SENDMSG = 9;
-static const int IORING_OP_RECVMSG = 10;
-static const int IORING_OP_TIMEOUT = 11;
-static const int IORING_OP_TIMEOUT_REMOVE = 12;
-static const int IORING_OP_ACCEPT = 13;
-static const int IORING_OP_ASYNC_CANCEL = 14;
-static const int IORING_OP_LINK_TIMEOUT = 15;
-static const int IORING_OP_CONNECT = 16;
-static const int IORING_OP_FALLOCATE = 17;
-static const int IORING_OP_OPENAT = 18;
-static const int IORING_OP_CLOSE = 19;
-static const int IORING_OP_FILES_UPDATE = 20;
-static const int IORING_OP_STATX = 21;
-static const int IORING_OP_READ = 22;
-static const int IORING_OP_WRITE = 23;
-static const int IORING_OP_FADVISE = 24;
-static const int IORING_OP_MADVISE = 25;
-static const int IORING_OP_SEND = 26;
-static const int IORING_OP_RECV = 27;
-static const int IORING_OP_OPENAT2 = 28;
-static const int IORING_OP_EPOLL_CTL = 29;
-static const int IORING_OP_SPLICE = 30;
-static const int IORING_OP_PROVIDE_BUFFERS = 31;
-static const int IORING_OP_REMOVE_BUFFERS = 32;
-static const int IORING_OP_TEE = 33;
-static const int IORING_OP_SHUTDOWN = 34;
-static const int IORING_OP_RENAMEAT = 35;
-static const int IORING_OP_UNLINKAT = 36;
-static const int IORING_OP_MKDIRAT = 37;
-static const int IORING_OP_SYMLINKAT = 38;
-static const int IORING_OP_LINKAT = 39;
-static const int IORING_OP_MSG_RING = 40;
-static const int IORING_OP_FSETXATTR = 41;
-static const int IORING_OP_SETXATTR = 42;
-static const int IORING_OP_FGETXATTR = 43;
-static const int IORING_OP_GETXATTR = 44;
-static const int IORING_OP_SOCKET = 45;
-static const int IORING_OP_URING_CMD = 46;
-static const int IORING_OP_SEND_ZC = 47;
-static const int IORING_OP_SENDMSG_ZC = 48;
-static const int IORING_OP_READ_MULTISHOT = 49;
-static const int IORING_OP_WAITID = 50;
-static const int IORING_OP_FUTEX_WAIT = 51;
-static const int IORING_OP_FUTEX_WAKE = 52;
-static const int IORING_OP_FUTEX_WAITV = 53;
-static const int IORING_OP_FIXED_FD_INSTALL = 54;
-static const int IORING_OP_FTRUNCATE = 55;
-static const int IORING_OP_BIND = 56;
-static const int IORING_OP_LISTEN = 57;
-static const int IORING_OP_RECV_ZC = 58;
-static const int IORING_OP_EPOLL_WAIT = 59;
-static const int IORING_OP_READV_FIXED = 60;
-static const int IORING_OP_WRITEV_FIXED = 61;
-static const int IORING_OP_PIPE = 62;
-static const int IORING_OP_NOP128 = 63;
-static const int IORING_OP_URING_CMD128 = 64;
-static const int IORING_OP_LAST = 65;
+int PROT_READ = 1;
+int PROT_WRITE = 2;
+int MAP_SHARED = 1;
+int MAP_PRIVATE = 2;
+int MAP_ANONYMOUS = 32;
+void *MAP_FAILED = (void *)-1;
 
-static const int PROT_READ = 1;
-static const int PROT_WRITE = 2;
-static const int MAP_SHARED = 1;
-static const int MAP_PRIVATE = 2;
-static const int MAP_ANONYMOUS = 32;
-static const void *MAP_FAILED = (void *)-1;
+unsigned long IORING_OFF_SQ_RING = 0;
+unsigned long IORING_OFF_CQ_RING = 134217728;
+unsigned long IORING_OFF_SQES = 268435456;
 
-static const unsigned long IORING_OFF_SQ_RING = 0;
-static const unsigned long IORING_OFF_CQ_RING = 134217728;
-static const unsigned long IORING_OFF_SQES = 268435456;
+unsigned IORING_ENTER_GETEVENTS = 1;
+*/
 
-static const unsigned IORING_ENTER_GETEVENTS = 1;
+int errno;
+struct Sync *global_sync;
 
-#pragma GCC diagnostic pop
-
-static int errno = 0;
-static struct Sync *global_sync = 0;
-
-static long raw_syscall(long sysno, long a0, long a1, long a2, long a3, long a4,
-			long a5) {
-	long result;
-	register long _a3 __asm__("r10") = a3;
-	register long _a4 __asm__("r8") = a4;
-	register long _a5 __asm__("r9") = a5;
-	__asm__ volatile("syscall"
-			 : "=a"(result)
-			 : "a"(sysno), "D"(a0), "S"(a1), "d"(a2), "r"(_a3),
-			   "r"(_a4), "r"(_a5)
-			 : "rcx", "r11", "memory");
-	return result;
+void memset(void *ptr, int value, unsigned long len) {
+	unsigned long i;
+	i = 0;
+loop:
+	if (i >= len) return;
+	((char *)ptr)[i++] = (unsigned char)value;
+	goto loop;
 }
 
-static int close(int fd) {
-	int ret = raw_syscall(3, fd, 0, 0, 0, 0, 0);
+long raw_syscall(long sysno, long a0, long a1, long a2, long a3, long a4,
+		 long a5);
+__asm(
+    ".section .text\n"
+    ".local raw_syscall\n"
+    "raw_syscall:\n"
+    "endbr64\n"
+    "mov    %rdi,%rax\n"
+    "mov    %r8,%r10\n"
+    "mov    %rsi,%rdi\n"
+    "mov    %r9,%r8\n"
+    "mov    %rdx,%rsi\n"
+    "mov    0x8(%rsp),%r9\n"
+    "mov    %rcx,%rdx\n"
+    "syscall\n"
+    "ret\n"
+    "xchg   %ax,%ax\n");
+
+int close(int fd) {
+	int ret;
+	ret = raw_syscall(3, fd, 0, 0, 0, 0, 0);
 	if (ret < 0) {
 		errno = -ret;
 		ret = -1;
@@ -198,9 +229,10 @@ static int close(int fd) {
 	return ret;
 }
 
-static void *mmap(void *addr, unsigned long length, int prot, int flags, int fd,
-		  long offset) {
-	void *ret =
+void *mmap(void *addr, unsigned long length, int prot, int flags, int fd,
+	   long offset) {
+	void *ret;
+	ret =
 	    (void *)raw_syscall(9, (long)addr, length, prot, flags, fd, offset);
 	if ((long)ret < 0) {
 		errno = -(long)ret;
@@ -208,8 +240,9 @@ static void *mmap(void *addr, unsigned long length, int prot, int flags, int fd,
 	}
 	return ret;
 }
-static int munmap(void *addr, unsigned long length) {
-	int ret = raw_syscall(11, (long)addr, length, 0, 0, 0, 0);
+int munmap(void *addr, unsigned long length) {
+	int ret;
+	ret = raw_syscall(11, (long)addr, length, 0, 0, 0, 0);
 	if (ret < 0) {
 		errno = -ret;
 		return -1;
@@ -219,21 +252,23 @@ static int munmap(void *addr, unsigned long length) {
 
 void exit_group(int status) {
 	raw_syscall(231, status, 0, 0, 0, 0, 0);
-	while (1);
+s:
+	goto s;
 }
 
-static int io_uring_setup(unsigned entries, struct io_uring_params *params) {
-	int ret = raw_syscall(425, entries, (long)params, 0, 0, 0, 0);
+int io_uring_setup(unsigned entries, struct io_uring_params *params) {
+	int ret;
+	ret = raw_syscall(425, entries, (long)params, 0, 0, 0, 0);
 	if (ret < 0) {
 		errno = -ret;
 		return -1;
 	}
 	return ret;
 }
-static int io_uring_enter2(unsigned fd, unsigned to_submit,
-			   unsigned min_complete, unsigned flags, void *arg,
-			   unsigned long sz) {
-	int ret =
+int io_uring_enter2(unsigned fd, unsigned to_submit, unsigned min_complete,
+		    unsigned flags, void *arg, unsigned long sz) {
+	int ret;
+	ret =
 	    raw_syscall(426, fd, to_submit, min_complete, flags, (long)arg, sz);
 	if (ret < 0) {
 		errno = -ret;
@@ -242,7 +277,7 @@ static int io_uring_enter2(unsigned fd, unsigned to_submit,
 	return 0;
 }
 
-static void sync_destroy(struct Sync *sync) {
+void sync_destroy(struct Sync *sync) {
 	if (sync) {
 		if (sync->sq_ring) munmap(sync->sq_ring, sync->sq_ring_size);
 		sync->sq_ring = 0;
@@ -258,12 +293,11 @@ static void sync_destroy(struct Sync *sync) {
 	}
 }
 
-static int sync_init(struct Sync **s) {
-	struct Sync *sync = 0;
+int sync_init(struct Sync **s) {
+	struct Sync *sync;
 
-	sync = mmap(0, sizeof(struct Sync), PROT_READ | PROT_WRITE,
-		    MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-	if (sync == MAP_FAILED) {
+	sync = mmap(0, sizeof(struct Sync), 3, 33, -1, 0);
+	if (sync == (void *)-1) {
 		return -1;
 	}
 
@@ -283,25 +317,23 @@ static int sync_init(struct Sync **s) {
 	    sync->params.cq_entries * sizeof(struct io_uring_cqe);
 	sync->sqes_size = sync->params.sq_entries * sizeof(struct io_uring_sqe);
 
-	sync->sq_ring = mmap(0, sync->sq_ring_size, PROT_READ | PROT_WRITE,
-			     MAP_SHARED, sync->ring_fd, IORING_OFF_SQ_RING);
-	if (sync->sq_ring == MAP_FAILED) {
+	sync->sq_ring = mmap(0, sync->sq_ring_size, 3, 1, sync->ring_fd, 0);
+	if (sync->sq_ring == (void *)-1) {
 		sync->sq_ring = 0;
 		sync_destroy(sync);
 		return -1;
 	}
 
-	sync->cq_ring = mmap(0, sync->cq_ring_size, PROT_READ | PROT_WRITE,
-			     MAP_SHARED, sync->ring_fd, IORING_OFF_CQ_RING);
+	sync->cq_ring =
+	    mmap(0, sync->cq_ring_size, 3, 1, sync->ring_fd, 134217728);
 
-	if (sync->cq_ring == MAP_FAILED) {
+	if (sync->cq_ring == (void *)-1) {
 		sync->cq_ring = 0;
 		sync_destroy(sync);
 		return -1;
 	}
-	sync->sqes = mmap(0, sync->sqes_size, PROT_READ | PROT_WRITE,
-			  MAP_SHARED, sync->ring_fd, IORING_OFF_SQES);
-	if (sync->sqes == MAP_FAILED) {
+	sync->sqes = mmap(0, sync->sqes_size, 3, 1, sync->ring_fd, 268435456);
+	if (sync->sqes == (void *)-1) {
 		sync->sqes = 0;
 		sync_destroy(sync);
 		return -1;
@@ -325,35 +357,36 @@ static int sync_init(struct Sync **s) {
 }
 
 void atomic_add_u32(unsigned *ptr, unsigned value);
-__asm__(
+__asm(
     ".section .text\n"
-    ".global atomic_add_u32\n"
+    ".local atomic_add_u32\n"
     "atomic_add_u32 :\n"
     "endbr64\n"
     "lock add %esi,(%rdi)\n"
     "ret\n");
 
 void atomic_sub_u32(unsigned *ptr, unsigned value);
-__asm__(
+__asm(
     ".section .text\n"
-    ".global atomic_sub_u32\n"
+    ".local atomic_sub_u32\n"
     "atomic_sub_u32 :\n"
     "endbr64\n"
     "lock sub %esi,(%rdi)\n"
     "ret\n");
 
-static long sync_execute(struct Sync *sync, const struct io_uring_sqe sqe) {
+long sync_execute(struct Sync *sync, struct io_uring_sqe sqe) {
 	int ret, result;
-	unsigned cq_mask = *sync->cq_mask;
-	unsigned sq_mask = *sync->sq_mask;
-	unsigned sq_tail = *sync->sq_tail;
-	unsigned index = sq_tail & sq_mask;
-	unsigned cq_head = *sync->cq_head;
-	unsigned idx, flag = IORING_ENTER_GETEVENTS;
+	unsigned cq_mask, sq_mask, sq_tail, index, cq_head, idx;
+
+	cq_mask = *sync->cq_mask;
+	sq_mask = *sync->sq_mask;
+	sq_tail = *sync->sq_tail;
+	index = sq_tail & sq_mask;
+	cq_head = *sync->cq_head;
 	sync->sq_array[index] = index;
 	sync->sqes[index] = sqe;
 	atomic_add_u32(sync->sq_tail, 1);
-	ret = io_uring_enter2(sync->ring_fd, 1, 1, flag, 0, 0);
+	ret = io_uring_enter2(sync->ring_fd, 1, 1, 1, 0, 0);
 
 	if (ret < 0)
 		atomic_sub_u32(sync->sq_tail, 1);
@@ -369,13 +402,15 @@ static long sync_execute(struct Sync *sync, const struct io_uring_sqe sqe) {
 		atomic_add_u32(sync->cq_head, 1);
 	}
 
-	return ret < 0 ? -1 : ret;
+	if (ret < 0) return -1;
+	return ret;
 }
 
-static int pwrite(int fd, const void *buf, unsigned long len,
-		  unsigned long offset) {
-	struct io_uring_sqe sqe = {0};
-	sqe.opcode = IORING_OP_WRITE;
+int pwrite(int fd, void *buf, unsigned long len, unsigned long offset) {
+	struct io_uring_sqe sqe;
+
+	memset(&sqe, 0, sizeof(sqe));
+	sqe.opcode = 23;
 	sqe.addr = (unsigned long)buf;
 	sqe.fd = fd;
 	sqe.len = len;
@@ -389,6 +424,8 @@ static int pwrite(int fd, const void *buf, unsigned long len,
 }
 
 int main(int argc, char **argv, char **envp) {
+	global_sync = 0;
+	errno = 0;
 	if (!argv || !envp) {
 		pwrite(2, "err!\n", 5, 0);
 		exit_group(-1);
@@ -397,7 +434,7 @@ int main(int argc, char **argv, char **envp) {
 	return argc;
 }
 
-__asm__(
+__asm(
     ".section .text\n"
     ".global _start\n"
     "_start:\n"
